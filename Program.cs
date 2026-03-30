cd818043e409c0453185ea84d7bdef377e1fa2dc
Paweł Gębicki <s32429@pjwstk.edu.pl>
2026-03-30 23:45:19 +0200

    Utworzenie obiektu klasy Osoba, wyświetlenie jego właściwości na konsoli

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace s32429_apbd_cw1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            try
+            {
+                var pawel = new Osoba("Paweł", "Kowalski", "90080375334");
+
+                Console.WriteLine($"Imię: {pawel.Imie}");
+                Console.WriteLine($"Nazwisko: {pawel.Nazwisko}");
+                Console.WriteLine($"PESEL: {pawel.Pesel}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
