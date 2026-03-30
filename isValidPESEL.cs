758ac4118cc0548a634b237646c5bdc446937012
Paweł Gębicki <s32429@pjwstk.edu.pl>
2026-03-30 23:38:39 +0200

    Dodanie do interfejsu sprawdzającego PESEL walidacji sumy kontrolnej

--- isValidPESEL.cs
+++ isValidPESEL.cs
@@ -11,6 +11,19 @@ namespace s32429_apbd_cw1
             if (!System.Text.RegularExpressions.Regex.IsMatch(PESEL, "^\\d{11}$"))
                 throw new ArgumentException("PESEL musi składać się z 11 cyfr.");
 
+
+            int[] weights = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+            int sum = 0;
+
+            for (int i = 0; i < weights.Length; i++)
+                sum += weights[i] * (PESEL[i] - '0');
+
+            int checksum = (10 - (sum % 10)) % 10;
+            int lastDigit = PESEL[10] - '0';
+
+            if(checksum != lastDigit)
+                throw new ArgumentException("Niepoprawna suma kontrolna PESEL.");
+
             return true;
         }
     }
